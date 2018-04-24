68fa5f788fdb07a576e099fb8e26de49a9dc0350
jsmond2016 <jsmond2016@gmail.com>
2018-04-24 15:27:52 +0800

    add score-animation

--- js/showanimation2048.js
+++ js/showanimation2048.js
@@ -6,21 +6,29 @@ function showNumberWithAnimation(i,j,randNum){
 	});
 	numberCell.text(randNum);
 	numberCell.animate({
-		width:cellSideLength,
-		height:cellSideLength,
-		'line-height':cellSideLength,
-		left:getPosLeft(i,j),
-		top:getPosTop(i,j)
+		width: cellSideLength,
+		height: cellSideLength,
+		'line-height': cellSideLength,
+		left: getPosLeft(i,j),
+		top: getPosTop(i,j)
 	},50);
 }
 function showMoveAnimation(fromx,fromy,tox,toy) {
     var  numberCell=$("#number-cell-"+fromx+"-"+fromy);
     numberCell.animate({
-      top:getPosTop(tox,toy),
-      left:getPosLeft(tox,toy)
+      top: getPosTop(tox,toy),
+      left: getPosLeft(tox,toy)
     },100);
 }
 
 function updateScore(score){
 	$("#score").text(score);
+	$("#score").animate({
+		'font-size': '1.2em',
+	},50,()=>{
+		$("#score").animate({
+			'font-size': '1em',
+		},50)
+	})
 }
+
